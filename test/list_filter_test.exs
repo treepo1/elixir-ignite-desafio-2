65270624a2278c8dd6c600c0158b968bbe218056
Thiago Freire <thiago.freire45@gmail.com>
2023-03-12 14:27:51 -0300

    add tests

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,19 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "count odd numeric elements" do
+      list1 = ["1","2","3"]
+      list2 = ["1","4","banana"]
+
+      assert ListFilter.call(list1) == 2
+      assert ListFilter.call(list2) == 1
+    end
+
+    test "passing an empty list" do
+      list = []
+
+      assert ListFilter.call(list) == 0
+    end
   end
 end
